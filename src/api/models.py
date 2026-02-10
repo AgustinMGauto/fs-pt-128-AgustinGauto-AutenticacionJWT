bdd02533f49262a951c3663621c3828eca32457c
AgustinMGauto <agustin_gauto@outlook.com>
2026-02-10 21:39:37 +0000

    en proceso

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(nullable=False)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from sqlalchemy import select
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +14,54 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/users', methods=[ 'GET'])
+def get_all_users():
+    all_users = User.query.all()
+    results = [user.serialize() for user in all_users]
+    return jsonify(results), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+@api.route('/register', methods=["POST"])
+def register():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+    if not email or not password:
+        return jsonify({"error": "email and password are required"}), 400
+    existing_user = db.session.execute(select(User).where(
+        User.email == email)).scalar_one_or_none()
+    if existing_user:
+        return jsonify({"error": "User with this email already exist"}), 400
+    new_user = User(email=email)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "user created successfully"}), 201
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+    if not email or not password:
+        return jsonify({"error": "Email and password are required"}), 400
+    user = db.session.execute(select(User).where(
+        User.email == email)).scalar_one_or_none()
+    if user is None:
+        return jsonify({"msg": "Email or password invalid"}), 400
+    if user.check_password(password):
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify({"msg": "Login successful, Welcome", "token": access_token}), 200
+    else:
+        return jsonify({"msg": "Email or password invalid"}), 400
+
+
+@api.route('/profile', methods=["GET"])
+@jwt_required()
+def get_profile():
+    user_id = get_jwt_identity()
+    user = db.session.get(User, int(user_id))
+    if not user:
+        return jsonify({"msg": "User not found"}), 400
+    return jsonify(user.serialize()), 200
